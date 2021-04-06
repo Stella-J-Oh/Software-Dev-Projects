ce90390278a31cd2ae6e0f62ae4122f8fd255bfa
Stella Oh <soh11@stuy.edu>
2021-04-06 18:23:22 -0400

    k19

--- 19_rest/app.py
+++ 19_rest/app.py
@@ -10,14 +10,17 @@ import urllib.request
 import json
 
 app = Flask(__name__)
+# read key_nasa.txt file 
+file = open("key_nasa.txt", "r")
+api_key = file.read()
 
 @app.route("/")
 def root():
-    u = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=QIi25sgr9icgfLI5qt4WinQ7opSZcDoXi2Lkgl2p")
-    response = u.read()
+    u = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=" + api_key) #open the api URL added with the key
+    response = u.read() #read the api
     data = json.loads(response)
-    return render_template("main.html", pic = data['url'])
+    return render_template("main.html", pic = data['url']) #load the html on the website
 
-if __name__ == "__main__": 
-    app.debug = True
+if __name__ == "__main__": # true is this file is NOT imported
+    app.debug = True #enable auto-reload upon code change
     app.run()
